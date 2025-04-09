7f1ea9318109a00b9d230b78db538c220e843ab6
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2025-04-09 21:15:47 +1000

    Add Travel menu
    Add Travel menu

---
+++ travelmenu.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.write("## NZ 여행  :red[북섬] ")  
+st.write ("### Auckland      ", "  (3박 4일 일정 ) "   ) 
+
+st.write("- 1 일차  Cathedral Cove, Hot Water Beach ","           3 시간거리 ")
+st.write("- 2 일차  Te Aroha Mineral Spa   ","          2.5 시간거리 ")
+st.write("- 3 일차  Piha Beach Black Sand  ","          1 시간거리 ")
+st.write("         Auckland Tower  ","         Auckland 시내 ")
+
+st.markdown(
+"""    
+  
+    [Presentation - 여행 알정       ](https://kevinoh-nztravel1.streamlit.app/)\n
+
+    [Video  -  Cathedral Cove   ](https://youtu.be/jSCW4RYP8lI)\n 
+    [Video  -  Piha Beach Black Sand   ](https://youtu.be/WWXiY0cxttI) 
+"""
+)  
